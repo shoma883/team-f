1c5e96bdee7e8267482848de287ace00711d50a9
shoma883 <shouma.s.0509@gmail.com>
2024-10-19 14:09:28 +0900

    test2

--- resources/views/inventories/index.blade.php
+++ resources/views/inventories/index.blade.php
@@ -1,34 +1,27 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{config('app.name')}}</title>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-    <style>
-        body {background: #bae6fd;}
-        h1 {margin: 30px 10px;}
-        h2 {margin: 30px 10px;}
-        form {display: flex; align-items: flex-end; margin: 0 0 20px 10px}
-        textarea {margin-right: 10px; padding: 5px 10px; width: 200px; height: 100px;}
-        button {padding: 5px 10px;height: 30px;}
-        p {margin: 5px 0 0 10px;}
-    </style>
-</head>
-<body>
-    <h1>{{config('app.name')}}</h1>
-    
-    <form action="{{route('entry')}}" method="post">
-        @csrf
-        <textarea name="toGeminiText" autofocus>@isset($result['task']){{$result['task']}}@endisset </textarea>
-        <button type="submit">send</button>
-    </form>
-    
-    <hr>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <form action="{{route('entry')}}" method="post">
+                    @csrf
+                    <textarea name="toGeminiText" autofocus>@isset($result['task']){{$result['task']}}@endisset </textarea>
+                    <button type="submit">send</button>
+                </form>
+                
+                <hr>
 
-    @isset($result)
-    <p>{!!$result['content']!!}</p>
-    @endisset
-</body>
-</html>
+                @isset($result)
+                <p>{!!$result['content']!!}</p>
+                @endisset
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
