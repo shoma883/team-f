d62536b3d8374de767daef3aa9ac12c1a423dcec
U5 <kyutech.joko@gmail.com>
2024-10-19 15:53:02 +0900

    inventoriesテーブル作成#11

--- database/migrations/2024_10_19_034755_create_inventories_table.php
+++ database/migrations/2024_10_19_034755_create_inventories_table.php
@@ -6,23 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('inventories', function (Blueprint $table) {
-            $table->id();
-            
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('inventories', function (Blueprint $table) {
+			$table->id();
+			$table->string('name');
+			$table->integer('stock');
+			$table->foreignId('user_id')->constrained()->cascadeOnDelete();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('inventories');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('inventories');
+	}
 };
