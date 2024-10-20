9a3d5895864fe918bfb5932fbd16c63c3875ccc8
U5 <kyutech.joko@gmail.com>
2024-10-20 12:04:20 +0900

    routeの追加#5

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,8 @@ Route::middleware('auth')->group(function () {
 
 	// Gemini Routes ===================================================================================
 	Route::get('/gemini', [GeminiController::class, 'index'])->name('gemini.index');
-	Route::post('/gemini', [GeminiController::class, 'entry'])->name('entry');
+	Route::post('/gemini', [GeminiController::class, 'entry'])->name('gemini.entry');
+	Route::post('/gemini/save', [GeminiController::class, 'save'])->name('gemini.save');
 });
 
 require __DIR__ . '/auth.php';
