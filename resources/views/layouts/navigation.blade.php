e352d2576e28414eb3edbeb814376ae95669fdfd
Yuki Muramoto <yuuki_610@icloud.com>
2024-10-20 10:51:02 +0900

    a

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,16 +15,14 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('inventories.stock')" :active="request()->routeIs('inventories.stock')">
-                        {{ __('在庫入力フォーム') }}
-                    </x-nav-link>
 
                     <x-nav-link :href="route('gemini.index')" :active="request()->routeIs('inventories.index')">
                         {{ __('検索') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('inventories.stock')" :active="request()->routeIs('inventories.stock')">
+                        {{ __('在庫入力フォーム') }}
                     </x-nav-link>
-
-
 
                     <x-nav-link :href="route('inventories.input')" :active="request()->routeIs('inventories.input')">
                         {{ __('食材入力フォーム') }}
@@ -85,18 +83,14 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
 
-            <x-responsive-nav-link :href="route('inventories.stock')" :active="request()->routeIs('inventories.stock')">
-                {{ __('在庫入力フォーム') }}
-            </x-responsive-nav-link>
-
             <!-- 🔽 1項目追加 -->
             <x-responsive-nav-link :href="route('gemini.index')" :active="request()->routeIs('inventories.index')">
                 {{ __('検索') }}
             </x-responsive-nav-link>
 
+            <x-responsive-nav-link :href="route('inventories.stock')" :active="request()->routeIs('inventories.stock')">
+                {{ __('在庫入力フォーム') }}
             </x-responsive-nav-link>
-
-
 
             <x-responsive-nav-link :href="route('inventories.input')" :active="request()->routeIs('inventories.input')">
                 {{ __('食材入力フォーム') }}
