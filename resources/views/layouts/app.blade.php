5e0f45db4d5f09fbc8ffc9de2193ff49e324236c
U5 <kyutech.joko@gmail.com>
2024-10-24 20:19:37 +0900

    コメントがコメントアウトできていない問題を解決

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,13 +26,13 @@
 </head>
 
 <body class="font-sans antialiased">
-  <div class="min-h-screen bg-gray-100 dark:bg-gray-900">@include('layouts.navigation') < !-- Page Heading -->
-      @isset($header)
-        <header class="bg-white dark:bg-gray-800 shadow">
-          <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{{ $header }} </div>
-        </header>
-      @endisset < !-- Page Content -->
-        <main>{{ $slot }} </main>
+  <div class="min-h-screen bg-gray-100 dark:bg-gray-900">@include('layouts.navigation') {{--  Page Heading --}}
+    @isset($header)
+      <header class="bg-white dark:bg-gray-800 shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{{ $header }} </div>
+      </header>
+    @endisset {{-- Page Content --}}
+    <main>{{ $slot }} </main>
   </div>
 </body>
 
